bf2789e863e259253e3da3f6b13670c4de64f19c
ReubenDev <reubenkiarie8m@gmail.com>
2023-04-21 16:27:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A NOTES APP WITH VANILLA JS
+
+This project is inspired by a notes app i wrote in Vue JS as I was learning Vue. I thought to mysefl, what if I wrote the same notes app using vanilla JS ? How challenging will it be?
---
+++ app.js
@@ -0,0 +1,5 @@
+//THIS IS WHAT THE APP WILL CONTAIN
+
+//A "New Note" button that opens a modal that will be used to create a note.
+//Inside that modal, a text field to input our data, an "Add Note" button to add the new note, & "close" button to close the modal
+//The app will be progressive, we might add local storage to save our note items, & other features.
